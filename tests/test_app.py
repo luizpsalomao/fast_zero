959d13b5d58534a4013c9ee975d558211b359096
luiz.salomao <luiz.salomao@pcdf.df.gov.br>
2026-01-12 23:11:29 -0300

    Configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient  
+
+from fast_zero.app import app  
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Olá Mundo!'}
+
